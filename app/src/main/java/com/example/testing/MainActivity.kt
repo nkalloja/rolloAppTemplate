d8c07bd9f126f64d65799653059da3b29980c970
nkalloja <84780504+nkalloja@users.noreply.github.com>
2021-06-04 16:01:26 +0300

    Buttonin animaatio
    Lisäksi päänäkymän muokkaus, animaatio kesken

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
+import android.media.Image
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,11 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.TextView;
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -39,6 +45,7 @@ class MainActivity : AppCompatActivity() {
             takePhoto()
         }
         this.transparentStatusBar()
+        animZoom()
     }
 
     fun Activity.transparentStatusBar() {
@@ -74,6 +81,83 @@ class MainActivity : AppCompatActivity() {
 //
 //        placeholderAdapter.submitItems(createItems())
 //    }
+
+    private fun animZoom() {
+        val zoom = AnimationUtils.loadAnimation(this, R.anim.zoom)
+        val zoom1 = AnimationUtils.loadAnimation(this, R.anim.zoom1)
+        val zoom2 = AnimationUtils.loadAnimation(this, R.anim.zoom2)
+        val zoom3 = AnimationUtils.loadAnimation(this, R.anim.zoom3)
+
+        var imgButton = findViewById(R.id.roundButton) as ImageView
+        var anim1 = findViewById(R.id.anim1) as ImageView
+        var anim2 = findViewById(R.id.anim2) as ImageView
+        var anim3 = findViewById(R.id.anim3) as ImageView
+
+
+
+        zoom.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                imgButton.alpha = 1f
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                animation?.start()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                Log.d(TAG, "onAnimationRepeat: ZOOM  REPEAT $animation")
+            }
+        })
+        zoom1.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                anim1.alpha = 1f
+                Log.d(TAG, "onAnimationStart: ZOOM 1 STARTED")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Log.d(TAG, "onAnimationStart: ZOOM1  END")
+                animation?.start()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+        })
+        zoom2.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                Log.d(TAG, "onAnimationStart: ZOOM 2 STARTED")
+                anim2.alpha = 1f
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Log.d(TAG, "onAnimationStart: ZOOM2  END")
+                animation?.start()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+        })
+        zoom3.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                Log.d(TAG, "onAnimationStart: ZOOM 3 STARTED")
+                anim3.alpha = 1f
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Log.d(TAG, "onAnimationStart: ZOOM3  END")
+                animation?.start()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+        })
+        imgButton.startAnimation(zoom)
+        anim1.startAnimation(zoom1)
+      //  anim2.startAnimation(zoom2)
+       // anim3.startAnimation(zoom3)
+    }
 
     private fun takePhoto() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
